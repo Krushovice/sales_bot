c7d82267f4df10122d69cf60201a177d3684f54b
Krushovice <Kickstar69@yandex.ru>
2024-02-12 05:34:52 +0000

    Build inline keyboards

--- app/api_v1/markups/inline_markup.py
+++ app/api_v1/markups/inline_markup.py
@@ -4,34 +4,73 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 def get_on_start_kb() -> InlineKeyboardMarkup:
     pay_btn = InlineKeyboardButton(
         text="Купить VPN 💰",
-        callback_data="buy"
+        callback_data="buy",
     )
     to_account_btn = InlineKeyboardButton(
         text="Аккаунт 👤",
-        callback_data="account"
+        callback_data="account",
     )
     help_btn = InlineKeyboardButton(
         text="Помощь ⚒",
-        callback_data="help"
+        callback_data="help",
     )
     advantage_btn = InlineKeyboardButton(
         text="Преимущества ♻️",
-        callback_data="advantage"
+        callback_data="advantage",
     )
 
     first_row = [pay_btn, help_btn]
     sec_row = [to_account_btn, advantage_btn]
 
-    rows = [first_row,
-            sec_row]
+    rows = [first_row, sec_row]
 
     markup = InlineKeyboardMarkup(inline_keyboard=rows)
     return markup
 
 
 def get_profile_kb() -> InlineKeyboardMarkup:
-    pass
+    refill_btn = InlineKeyboardButton(
+        text="Пополнить баланс 💰",
+        callback_data="refill_balance",
+    )
+    to_main_btn = InlineKeyboardButton(
+        text="Назад 🔙",
+        callback_data="back_main",
+    )
+
+    first_row = [refill_btn]
+    sec_row = [to_main_btn]
+
+    rows = [first_row, sec_row]
+
+    markup = InlineKeyboardMarkup(inline_keyboard=rows)
+    return markup
 
 
 def get_payment_kb() -> InlineKeyboardMarkup:
-    pass
+
+    first_btn = InlineKeyboardButton(
+        text="130 - 1мес",
+        callback_data="one_month",
+    )
+    sec_btn = InlineKeyboardButton(
+        text="400 - 3мес",
+        callback_data="three_months",
+    )
+    third_btn = InlineKeyboardButton(
+        text="800 - 6мес",
+        callback_data="six_months",
+    )
+    back_btn = InlineKeyboardButton(
+        text="Назад🔙",
+        callback_data="back_to_account",
+    )
+
+    first_row = [first_btn]
+    sec_row = [sec_btn]
+    third_row = [third_btn]
+    last_row = [back_btn]
+    rows = [first_row, sec_row, third_row, last_row]
+
+    markup = InlineKeyboardMarkup(inline_keyboard=rows)
+    return markup
--- app/api_v1/routers/commands/main_commands.py
+++ app/api_v1/routers/commands/main_commands.py
@@ -2,7 +2,7 @@ from aiogram.filters import Command, CommandStart
 from aiogram.utils import markdown
 from aiogram.types import Message
 from aiogram import Router
-from app.api_v1.markups import get_on_start_kb
+from app.api_v1.markups import get_on_start_kb, get_profile_kb
 from app.api_v1.utils.lexicon import LEXICON_RU
 
 from app.api_v1.core.crud import AsyncOrm
@@ -16,7 +16,7 @@ async def command_start_handler(message: Message):
     await AsyncOrm.create_user(
         tg_id=message.from_user.id,
         username=message.from_user.username,
-        )
+    )
 
     await message.answer(
         text=markdown.bold(LEXICON_RU["/start"]),
@@ -31,9 +31,20 @@ async def command_help_handler(message: Message):
 
 @router.message(Command("account", prefix="!/"))
 async def show_profile_handler(message: Message):
-    user = await AsyncOrm.get_user(tg_id=message.from_user.id,
-                                   username=message.from_user.username)
-    await message.answer(text=f"Привет, {message.from_user.username}")  #reply_markup=get_profile_kb())
+    user = await AsyncOrm.get_user(
+        tg_id=message.from_user.id,
+        username=message.from_user.username,
+    )
+    await message.answer(
+        text=(
+            f"<b>Личный кабинет</b>\n\n"
+            f"🆔 {user.tg_id} \n"
+            f"💰 Баланс: {user.balance}руб\n\n"
+            f"<i>Для оплаты и продления VPN используется баланс.\n</i>"
+            f"<i>Для его пополнения используйте клавиши ниже</i>"
+        ),
+        reply_markup=get_profile_kb(),
+    )
 
 
 @router.message(Command("payment", prefix="!/"))
@@ -41,15 +52,18 @@ async def refill_user_balance(message: Message):
     user = await AsyncOrm.update_user(
         tg_id=message.from_user.id,
         cost=129,
-        balance=200)
+        balance=200,
+    )
 
-    await message.answer(text=f"""Оплата прошла успешно!
+    await message.answer(
+        text=f"""Оплата прошла успешно!
 Стоимость услуги: {user.cost},
-Ваш баланс: {user.balance}""")
+Ваш баланс: {user.balance}"""
+    )
     updated_user = await AsyncOrm.update_user(
         tg_id=message.from_user.id,
         subscription=True,
-        )
+    )
     if updated_user.subscription:
         await message.answer("Подписка оформлена!")
     else:
