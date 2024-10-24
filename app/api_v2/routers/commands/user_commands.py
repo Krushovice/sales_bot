d6f366c9bc066092eda6b09206f6e7c81687a6a7
Krushovice <kickstar69@yandex.ru>
2024-10-24 17:34:36 +1000

    setup user commands logger

--- app/api_v2/routers/commands/user_commands.py
+++ app/api_v2/routers/commands/user_commands.py
@@ -12,16 +12,25 @@ from app.api_v1.utils import outline_helper
 
 from app.api_v1.config import settings
 
+from app.api_v1.utils.logging import setup_logger
+from app.api_v2.routers.callback_handlers.main_menu_callback_handlers import logger
+
 router = Router(name=__name__)
+
+my_logger = setup_logger(__name__)
 
 file_path = "app/api_v1/utils/images/image2.jpg"
 
 
-@router.message(F.text == "REALVPN2024CPG")
-@router.message(F.text == "LEVITSKAYA")
+@router.message(F.text.in_({"REALVPN2024CPG", "LEVITSKAYA"}))
 async def user_promo_handler(message: Message) -> None:
     tg_id = message.from_user.id
     user = await AsyncOrm.get_user(tg_id=tg_id)
+    if not user:
+        user = await AsyncOrm.create_user(
+            tg_id=tg_id,
+            username=message.from_user.username,
+        )
     if user.key:
         await message.answer_photo(
             photo=FSInputFile(
@@ -31,28 +40,31 @@ async def user_promo_handler(message: Message) -> None:
             reply_markup=root_kb(),
         )
 
-    if not user:
-        user = await AsyncOrm.create_user(
-            tg_id=tg_id,
-            username=message.from_user.username,
-        )
     today = datetime.now()
     if message.text == "REALVPN2024CPG":
         delta = timedelta(days=7)
     elif message.text == "LEVITSKAYA":
         delta = timedelta(days=10)
-        if not await AsyncOrm.get_referrer(tg_id=tg_id):
+        try:
+            referrer = await AsyncOrm.get_referrer(tg_id=tg_id)
+        except Exception as e:
+            referrer = None
+            # Логирование ошибки
+            my_logger.error(f"Ошибка при получении реферрала для пользователя {tg_id}: {e}")
+
+        if not referrer:
             await AsyncOrm.update_user(
                 tg_id=int(settings.ADVERTISER_ID),
                 referral=user,
             )
-        with open("count_referrals.txt", "w+") as f:
+        with open("count_referrals.txt", "r+") as f:
             count = f.read()
             if not count:
                 f.write("1")
             else:
                 count = int(count)
                 count += 1
+                f.seek(0)
                 f.write(str(count))
 
     expiration_date = (today + delta).strftime("%d-%m-%Y")
